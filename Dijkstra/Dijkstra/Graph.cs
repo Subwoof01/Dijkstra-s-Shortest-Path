fed9177c5d51db5e803fca5825dd663a61dd558f
Jordan <SubwoofGaming@gmail.com>
2018-09-23 14:34:57 +0200

    master-commit

---
+++ Dijkstra/Dijkstra/Graph.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dijkstra
+{
+    class Graph
+    {
+        // Dictionary that holds the graph. Format: A, {B, 7}, {C, 8}
+        Dictionary<char, Dictionary<char, int>> Edges = new Dictionary<char, Dictionary<char, int>>();
+
+        /// <summary>
+        /// Adds an edge to the graph.
+        /// </summary>
+        /// <param name="name">The name of the node.</param>
+        /// <param name="paths">The paths you can take from this node.</param>
+        public void AddEdge(char name, Dictionary<char, int> paths)
+        {
+            Edges[name] = paths;
+        }
+
+        public List<char> FindShortestPath(char start, char end)
+        {
+            // List of previous nodes.
+            Dictionary<char, char> previous = new Dictionary<char, char>();
+            // List to keep the distances to and from nodes.
+            Dictionary<char, int> distances = new Dictionary<char, int>();
+            // List of all nodes in graph.
+            List<char> nodes = new List<char>();
+
+            // List of shortest path.
+            List<char> path = null;
+
+            // Set start distance to 0 and distances to all other nodes to infinity (max int value).
+            foreach (var edge in Edges)
+            {
+                if (edge.Key == start)
+                    distances[edge.Key] = 0;
+                else
+                    distances[edge.Key] = int.MaxValue;
+
+                // Add node to list of nodes in graph.
+                nodes.Add(edge.Key);
+            }
+
+            // As long as there are nodes to explore do...
+            while (nodes.Count != 0)
+            {
+                // Sort nodes by distance.
+                nodes.Sort((i, j) => distances[i] - distances[j]);
+
+                // Set the shortest distance node to smallest.
+                char smallest = nodes[0];
+                
+                // If the smallest node is the end, set Path = to the path taken.
+                if (smallest == end)
+                {
+                    path = new List<char>();
+                    while (previous.ContainsKey(smallest))
+                    {
+                        path.Add(smallest);
+                        smallest = previous[smallest];
+                    }
+
+                    break;
+                }
+
+                // Prevent infitine loops if I goofed up inputting the graph.
+                if (distances[smallest] == int.MaxValue)
+                {
+                    break;
+                }
+
+
+                nodes.Remove(smallest);
+
+                foreach (var neighbor in Edges[smallest])
+                {
+                    var alt = distances[smallest] + neighbor.Value;
+                    if (alt < distances[neighbor.Key])
+                    {
+                        distances[neighbor.Key] = alt;
+                        previous[neighbor.Key] = smallest;
+                    }
+                }
+            }
+
+            return path;
+        }
+    }
+}
+
+// --PSEUDOCODE--
+// 1:	function Dijkstra(Graph, source):
+// 2:	    for each vertex v in Graph:	                 Initialization
+// 3:	        dist[v] := infinity	                     initial distance from source to vertex v is set to infinite
+// 4:	        previous[v] := undefined	             Previous node in optimal path from source
+// 5:	    dist[source] := 0	                         Distance from source to source
+// 6:	    Q := the set of all nodes in Graph	         all nodes in the graph are unoptimized - thus are in Q
+// 7:	    while Q is not empty:	                     main loop
+// 8:	        u := node in Q with smallest dist[]
+// 9:	        remove u from Q
+// 10:	        for each neighbor v of u:	             where v has not yet been removed from Q.
+// 11:	            alt := dist[u] + dist_between(u, v)
+// 12:	            if alt<dist[v]	                     Relax (u,v)
+// 13:	                dist[v] := alt
+// 14:	                previous[v] := u
+// 15:	    return previous[]
---
+++ Dijkstra/Dijkstra/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dijkstra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // See Graph.png for visual representation of the graph.
+            Graph g = new Graph();
+            g.AddEdge('A', new Dictionary<char, int>() { { 'B', 1 }, { 'C', 6 }, { 'D', 5 } });
+            g.AddEdge('B', new Dictionary<char, int>() { { 'A', 1 }, { 'C', 6 } });
+            g.AddEdge('C', new Dictionary<char, int>() { { 'A', 6 }, { 'B', 6 }, { 'F', 3 }, { 'E', 7 } });
+            g.AddEdge('D', new Dictionary<char, int>() { { 'A', 5 }, { 'G', 10 }, { 'F', 2 } });
+            g.AddEdge('E', new Dictionary<char, int>() { { 'C', 7 }, { 'H', 12 } });
+            g.AddEdge('F', new Dictionary<char, int>() { { 'C', 3 }, { 'D', 2 }, { 'H', 8 } });
+            g.AddEdge('G', new Dictionary<char, int>() { { 'D', 10 }, { 'H', 7 }, { 'I', 3 } });
+            g.AddEdge('H', new Dictionary<char, int>() { { 'E', 12 }, { 'F', 8 }, { 'G', 7 }, { 'I', 8 } });
+            g.AddEdge('I', new Dictionary<char, int>() { { 'G', 3 }, { 'H', 8 } });
+
+            List<char> shortestPath = g.FindShortestPath('A', 'I');
+
+            for (int i = shortestPath.Count - 1; i >= 0; i--)
+            {
+                Console.WriteLine(shortestPath[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
